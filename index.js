fcacb9bb1ca82eb728004adca567bc4642c78f7e
Phil Wekullo <philwekullo@gmail.com>
2023-12-13 22:12:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+window.customerName = 'bob'
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob'
+    
+}
+
+function overwriteBestCustomer () {
+    window.bestCustomer = 'maybe bob'
+    
+}
+
+const leastFavoriteCustomer = 'Tekashi'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Not Tekashi'
+}
